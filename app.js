b53a6c23480e9d9f9f52957e965788e38ecfd4dc
bickyy <vicky.zhou21@sitechhs.com>
2020-09-16 20:34:47 -0400

    work

---
+++ app.js
@@ -0,0 +1,23 @@
+// old way
+var life = 100; // this is the life bar NUMBER
+
+life = 50; // declare a new value
+
+life = life - 10; // another new value but with math :o
+
+var name = "make sure it is in quotes"; // name displayed in game (String)
+
+var checkout = false; //Boolean
+var box; // undefined (no value in variable)
+var box = null; // has a value of nothing
+
+// new way
+const life = 100;
+life = 50; // this doesn't work it: wont change unlike var
+// once const is declared you cant change it
+const life = 100; // cannot redeclare like this
+
+let life = 100; // if you want to change variable use let
+life = life - 50;
+
+console.log("life");
